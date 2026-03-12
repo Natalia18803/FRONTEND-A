6b3dc9632d2e6718b6e279753528c5cf9879e6e5
Natalia <natisss820@gmail.com>
2026-03-11 19:38:16 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+// Rutas manejadas por vue-router
+</script>
+
+<style lang="scss">
+// Variables de color globales
+$primary: #3211d4;
+$mystic-gold: #c5a059;
+$background-dark: #131022;
+
+body {
+  font-family: 'Inter', sans-serif;
+  margin: 0;
+  padding: 0;
+  background: $background-dark;
+  color: white;
+}
+</style>
